737f66e72a4aae6df7b9bd5cec17f773c863813d
siglyph <judecobler@icloud.com>
2025-11-08 21:21:43 -0600

    Initial Commit

---
+++ src/main/java/net/kobura/koburasempires/KoburasEmpiresModClient.java
@@ -0,0 +1,10 @@
+package net.kobura.koburasempires;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KoburasEmpiresModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kobura/koburasempires/KoburasEmpiresModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kobura.koburasempires;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KoburasEmpiresModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
