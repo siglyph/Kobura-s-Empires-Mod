2d3e37c94f24c65d504e6a903b853532246600f3
siglyph <judecobler@icloud.com>
2025-11-14 00:23:53 -0600

    Add BanditModel
    - Added Bandit's model in-game
    - No animations yet

--- src/main/java/net/kobura/koburasempires/entity/client/BanditModel.java
+++ src/main/java/net/kobura/koburasempires/entity/client/BanditModel.java
@@ -8,19 +8,15 @@ import net.minecraft.client.util.math.MatrixStack;
 
 public class BanditModel extends SinglePartEntityModel<BanditEntity> {
 
-    private ModelPart Waist;
-    private ModelPart Head;
-    private ModelPart Body;
-    private ModelPart RightArm;
-    private ModelPart LeftArm;
-    private ModelPart RightLeg;
-    private ModelPart LeftLeg;
+    private final ModelPart Waist;
+    private final ModelPart Head;
+    private final ModelPart Body;
+    private final ModelPart RightArm;
+    private final ModelPart LeftArm;
+    private final ModelPart RightLeg;
+    private final ModelPart LeftLeg;
 
-    public BanditModel(ModelPart part) {
-        this.Body = part;
-    }
-
-    public void set (ModelPart root) {
+    public BanditModel(ModelPart root) {
         this.Waist = root.getChild("Waist");
         this.Head = this.Waist.getChild("Head");
         this.Body = this.Waist.getChild("Body");
@@ -29,21 +25,10 @@ public class BanditModel extends SinglePartEntityModel<BanditEntity> {
         this.RightLeg = root.getChild("RightLeg");
         this.LeftLeg = root.getChild("LeftLeg");
     }
-
-    public BanditModel(ModelPart part, ModelPart waist, ModelPart head, ModelPart body, ModelPart rightArm, ModelPart leftArm, ModelPart rightLeg, ModelPart leftLeg) {
-        Waist = waist;
-        Head = head;
-        Body = body;
-        RightArm = rightArm;
-        LeftArm = leftArm;
-        RightLeg = rightLeg;
-        LeftLeg = leftLeg;
-    }
-
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
-        ModelPartData Waist = modelPartData.addChild("Waist", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 12.0F, 0.0F));
+        ModelPartData Waist = modelPartData.addChild("Waist", ModelPartBuilder.create(), ModelTransform.of(0.0F, 12.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
 
         ModelPartData Head = Waist.addChild("Head", ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.0F))
                 .uv(32, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.5F)), ModelTransform.of(0.0F, -12.0F, 0.0F, -0.1047F, 0.0873F, 0.0F));
@@ -58,17 +43,15 @@ public class BanditModel extends SinglePartEntityModel<BanditEntity> {
                 .uv(48, 48).cuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.of(5.0F, -10.0F, 0.0F, 0.2094F, 0.0F, 0.0F));
 
         ModelPartData RightLeg = modelPartData.addChild("RightLeg", ModelPartBuilder.create().uv(0, 16).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F))
-                .uv(0, 32).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.of(-1.9F, 12.0F, 0.0F, 0.192F, 0.0F, 0.0349F));
+                .uv(0, 32).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.of(-1.9F, 12.0F, 0.0F, 2.9496F, 0.0F, -3.1067F));
 
         ModelPartData LeftLeg = modelPartData.addChild("LeftLeg", ModelPartBuilder.create().uv(16, 48).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F))
-                .uv(0, 48).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.of(1.9F, 12.0F, 0.0F, -0.1745F, 0.0F, -0.0349F));
+                .uv(0, 48).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.of(1.9F, 12.0F, 0.0F, -2.9671F, 0.0F, 3.1067F));
         return TexturedModelData.of(modelData, 64, 64);
     }
-    @Override
-    public void setAngles(BanditEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-    }
 
-    public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha, int color) {
+    @Override
+    public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, int color) {
         Waist.render(matrices, vertexConsumer, light, overlay, color);
         RightLeg.render(matrices, vertexConsumer, light, overlay, color);
         LeftLeg.render(matrices, vertexConsumer, light, overlay, color);
@@ -77,5 +60,10 @@ public class BanditModel extends SinglePartEntityModel<BanditEntity> {
     @Override
     public ModelPart getPart() {
         return Body;
+    }
+
+    @Override
+    public void setAngles(BanditEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
+
     }
 }
