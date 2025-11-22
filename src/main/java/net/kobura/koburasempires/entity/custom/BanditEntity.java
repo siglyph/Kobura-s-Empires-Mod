53843b72098dbe08cb89678891d4afbcf147ca1c
siglyph <judecobler@icloud.com>
2025-11-21 23:32:45 -0600

    11.21.25

--- src/main/java/net/kobura/koburasempires/entity/custom/BanditEntity.java
+++ src/main/java/net/kobura/koburasempires/entity/custom/BanditEntity.java
@@ -1,15 +1,22 @@
 package net.kobura.koburasempires.entity.custom;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.*;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.raid.RaiderEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.RangedWeaponItem;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.World;
 
 public class BanditEntity extends PathAwareEntity {
@@ -20,10 +27,10 @@ public class BanditEntity extends PathAwareEntity {
     public static DefaultAttributeContainer.Builder createBanditAttributes(){
         return MobEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 20.0D)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25D)
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 1);
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.35F)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 1)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 100.0);
     }
-
 
     @Override
     protected void initGoals() {
@@ -34,8 +41,26 @@ public class BanditEntity extends PathAwareEntity {
         this.goalSelector.add(10, new LookAtEntityGoal(this, MobEntity.class, 15.0F));
 
         this.goalSelector.add(1, new MeleeAttackGoal(this, 1.0, true));
-        this.targetSelector.add(1, new ActiveTargetGoal<>(this, HostileEntity.class, true));
+        this.goalSelector.add(1, new RevengeGoal(this));
+
+        // Mob will target: Hostile mobs, players, merchants, iron golems, and villagers.
+        this.targetSelector.add(5, new ActiveTargetGoal<>(this, HostileEntity.class, true));
+        this.targetSelector.add(1, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
+        this.targetSelector.add(1, new ActiveTargetGoal<>(this, MerchantEntity.class, true));
+        this.targetSelector.add(1, new ActiveTargetGoal<>(this, IronGolemEntity.class, true));
+        this.targetSelector.add(1, new ActiveTargetGoal<>(this, VillagerEntity.class, true));
     }
+
+    @Override
+    public boolean canUseRangedWeapon(RangedWeaponItem weapon) {
+        return weapon == Items.CROSSBOW;
+    }
+
+    @Override
+    protected void initEquipment(Random random, LocalDifficulty localDifficulty) {
+        this.equipStack(EquipmentSlot.MAINHAND, new ItemStack(Items.NETHERITE_SWORD));
+    }
+
     @Override
     public void tick() {
         super.tick();
