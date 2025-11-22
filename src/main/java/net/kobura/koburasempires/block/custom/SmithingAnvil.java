71dcf2625a738191f9aa9532d8c531c56758057e
siglyph <judecobler@icloud.com>
2025-11-21 18:46:11 -0600

    ddd

--- src/main/java/net/kobura/koburasempires/block/custom/SmithingAnvil.java
+++ src/main/java/net/kobura/koburasempires/block/custom/SmithingAnvil.java
@@ -1,14 +1,25 @@
 package net.kobura.koburasempires.block.custom;
 
+import net.kobura.koburasempires.entity.ModEntities;
+import net.kobura.koburasempires.entity.custom.BanditEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.Objects;
 
 public class SmithingAnvil extends Block {
 
@@ -19,16 +30,22 @@ public class SmithingAnvil extends Block {
 
     // Play sound on right click
     @Override
-    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
 
-        if (world.isDay()){
-            world.playSound(player, pos, SoundEvents.BLOCK_ANVIL_PLACE, SoundCategory.BLOCKS, 10.0F, 1.0F);
-        }
-        else {
-            world.playSound(player, pos, SoundEvents.ENTITY_CAT_PURR, SoundCategory.NEUTRAL, 10.0F, 1.0F);
-        }
-
+        // player.sendMessage(Text.literal("Day: " + world.isDay() + " Night: " + world.isNight() + "Is raining: " + world.isRaining()), true);
+        world.playSound(null, pos, SoundEvents.BLOCK_ANVIL_USE, SoundCategory.BLOCKS, 1.0F, 1.0F);
 
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void onDestroyedByExplosion(World world, BlockPos pos, Explosion explosion) {
+
+        if (world instanceof ServerWorld serverWorld){
+            for (int i = 0; i < 10; i++)
+                ModEntities.BANDIT.spawn(serverWorld, pos, SpawnReason.EVENT);
+        }
+
+        super.onDestroyedByExplosion(world, pos, explosion);
     }
 }
--- src/main/java/net/kobura/koburasempires/entity/custom/BanditEntity.java
+++ src/main/java/net/kobura/koburasempires/entity/custom/BanditEntity.java
@@ -4,12 +4,12 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.mob.*;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.raid.RaiderEntity;
+import net.minecraft.text.Text;
 import net.minecraft.world.World;
 
 public class BanditEntity extends PathAwareEntity {
@@ -32,10 +32,22 @@ public class BanditEntity extends PathAwareEntity {
         this.goalSelector.add(8, new WanderAroundGoal(this, 0.6));
         this.goalSelector.add(9, new LookAtEntityGoal(this, PlayerEntity.class,15.0F, 1.0F));
         this.goalSelector.add(10, new LookAtEntityGoal(this, MobEntity.class, 15.0F));
-        this.targetSelector.add(1, new RevengeGoal(this, RaiderEntity.class).setGroupRevenge());
-        this.targetSelector.add(2, new ActiveTargetGoal(this, PlayerEntity.class, true));
-        this.targetSelector.add(3, new ActiveTargetGoal(this, MerchantEntity.class, false));
-        this.targetSelector.add(3, new ActiveTargetGoal(this, IronGolemEntity.class, true));
+
+        this.goalSelector.add(1, new MeleeAttackGoal(this, 1.0, true));
+        this.targetSelector.add(1, new ActiveTargetGoal<>(this, HostileEntity.class, true));
     }
+    @Override
+    public void tick() {
+        super.tick();
+        if (!this.getWorld().isClient) {
+            if (this.getTarget() != null) {
+                // super jank debug, but useful
+                this.getWorld().getPlayers().forEach(p ->
+                        p.sendMessage(Text.literal("Bandit targeting: " + this.getTarget().getName().getString()), true)
+                );
+            }
+        }
+    }
+
 
 }
